988f0404056f803857c108e8152e608944b71f0a
Migle <mig.navickaite@gmail.com>
2018-11-26 22:47:37 +0200

    Pridėtas namų darbas PHP3

---
+++ php03/index.php
@@ -0,0 +1,30 @@
+<?php
+$x = 10;
+$y = 7;
+$z = $x + $y
+ ?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+   <?php
+    echo "$x + $y = $z";
+    echo '<br>';
+     $z = $x - $y;
+    echo "$x - $y = $z";
+    echo '<br>';
+     $z = $x * $y;
+    echo "$x * $y = $z";
+    echo '<br>';
+     $z = $x / $y;
+    echo "$x / $y = $z";
+    echo '<br>';
+     $z = $x % $y;
+    echo "$x % $y = $z";  ?></p>
+
+  </body>
+</html>
