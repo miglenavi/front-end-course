f21a8de40e1f1a1487ab2ecadfd3d21e531d0493
Migle <mig.navickaite@gmail.com>
2018-11-26 22:43:29 +0200

    Pridėtas namų darbas PHP2

---
+++ php02/index.php
@@ -0,0 +1,19 @@
+<?php
+$country = 'Lietuva';
+$city = 'Vilnius';
+$population = 574221;
+$largest_population = "true";
+ ?>
+
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <p> <?php echo $city . ' - Lietuvos sostinė'; ?> </p>
+    <p> <?php echo "$city ir $country sparčiai auga" ?> </p>
+  </body>
+</html>
