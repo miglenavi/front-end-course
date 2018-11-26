4e08d609e3cd59c8bc99f7b0d67096cb70c67477
Migle <mig.navickaite@gmail.com>
2018-11-26 22:41:22 +0200

    Pridėtas namų darbas PHP1

---
+++ php01/index.php
@@ -0,0 +1,20 @@
+<?php
+$country = 'Lietuva';
+$city = 'Vilnius';
+$population = 574221;
+ ?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <ul>
+      <li><?php echo $country; ?></li>
+      <li><?php echo $city; ?></li>
+      <li><?php echo $population; ?></li>
+    </ul>
+  </body>
+</html>
