02d68be58be767386e2ee9cfc070fcce2ae6531d
Migle <mig.navickaite@gmail.com>
2018-11-29 15:46:44 +0200

    Pridėtas namų darbas PHP04

--- php04/index.php
+++ php04/index.php
@@ -1,13 +1,19 @@
 <?php
+// The first task
+$cities =['Berlynas', 'Roma', 'Londonas'];
+// The second task
+print_r($cities);
+//The forth task
+$cities[] = 'Tokijas';
+//The fifth task
 $cities2 =['Tokijas','Vasingtonas', 'Maskva'];
 $cities2 =[
   'Tokijas' => 13.6,
   'Vasingtonas' => 0.6,
   'Maskva' => 11.5
 ];
+//The sixt task
 $cities2['Londonas'] = 8.6;
-print_r($cities2);
-
  ?>
 
  <!DOCTYPE html>
@@ -17,8 +23,14 @@ print_r($cities2);
      <title></title>
    </head>
    <body>
+     <!-- The third task  -->
+     <ul>
+       <li><?php echo $cities[1]; ?></li>
+     </ul>
+     <!-- The seventh task  -->
      <ul>
        <li><?php echo "Gyventojų skaičius: $cities2[Tokijas] mln."; ?></li>
      </ul>
+
    </body>
  </html>
