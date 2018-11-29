52e6b02726ef5bbec3e30fc87d44117950db6d0c
Migle <mig.navickaite@gmail.com>
2018-11-29 22:43:56 +0200

    Pridėtas namų darbas PHP7

---
+++ php07/index.php
@@ -0,0 +1,28 @@
+<?php
+//The first task
+$cities4 =['Tokijas', 'Vašingtonas', 'Maskva', 'Londonas'];
+ ?>
+
+ <!DOCTYPE html>
+ <html lang="en" dir="ltr">
+   <head>
+     <meta charset="utf-8">
+     <title></title>
+   </head>
+   <body>
+     <!-- The second and third task -->
+     <ul>
+       <?php
+       for($i = 0; $i < count($cities4); $i++) {
+         echo "<li>$cities4[$i]</li>";
+       }
+        ?>
+     </ul>
+     <!--The forth task -->
+     <ul>
+       <?php foreach ($cities4 as $cities4){
+         echo "<li>$cities4</li>";
+       } ?>
+     </ul>
+   </body>
+ </html>
