d34cbc74505bfe8652b42faac0398f75ff3cd2e3
Migle <mig.navickaite@gmail.com>
2018-12-03 01:23:16 +0200

    Pataisiau PHP11

--- php11/index.php
+++ php11/index.php
@@ -9,7 +9,14 @@
     if($_REQUEST['ilgis'] && $_REQUEST['plotis']){
       $plotas = $_REQUEST['ilgis'] * $_REQUEST['plotis'];
       echo "Jūsų stačiakampio plotas yra $plotas cm";
-    } else { ?>
+    } elseif ($_REQUEST['ilgis'] || $_REQUEST['plotas']){
+      echo "<p>Prašau įvesti informaciją</p>";?>
+      <form action="<?php $_PHP_SELF; ?>" method="POST">
+        Ilgis: <input type="number" name="ilgis" />
+        Plotis: <input type="number" name="plotis" />
+        <input type="submit" name="submit" value="submit">
+      </form> <?php
+    } else{ ?>
       <form action="<?php $_PHP_SELF; ?>" method="POST">
         Ilgis: <input type="number" name="ilgis" />
         Plotis: <input type="number" name="plotis" />
