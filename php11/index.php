9f7d63debe4023ae0d8f827540ab51162787b2c2
Migle <mig.navickaite@gmail.com>
2018-12-02 23:20:51 +0200

    Pridėtas namų darbas PHP11

---
+++ php11/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php
+    if($_REQUEST['ilgis'] && $_REQUEST['plotis']){
+      $plotas = $_REQUEST['ilgis'] * $_REQUEST['plotis'];
+      echo "Jūsų stačiakampio plotas yra $plotas cm";
+    } else { ?>
+      <form action="<?php $_PHP_SELF; ?>" method="POST">
+        Ilgis: <input type="number" name="ilgis" />
+        Plotis: <input type="number" name="plotis" />
+        <input type="submit" name="submit" value="submit">
+      </form>
+      <?php
+    }
+     ?>
+  </body>
+</html>
