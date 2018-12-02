95d51c986f7a8ba6b993679f4469162fbc149661
Migle <mig.navickaite@gmail.com>
2018-12-03 01:22:15 +0200

    Pataisiau PHP12

--- php12/index.php
+++ php12/index.php
@@ -9,15 +9,21 @@
     if($_REQUEST['name'] && $_REQUEST['surname']){
       $name = $_REQUEST['name'];
       $surname = $_REQUEST['surname'];
-      echo $name . '<br>' . $surname;
-    } else { ?>
+      echo "$name $surname";
+    } elseif($_REQUEST['name'] || $_REQUEST['surname']){
+      echo "<p>Prašau įvesti informaciją</p>";?>
+      <form action="<?php $_PHP_SELF; ?>" method="POST">
+        Vardas: <input type="text" name="name" />
+        Pavardė: <input type="text" name="surname" />
+        <input type="submit" name="submit" value="submit">
+      </form><?php
+    }  else { ?>
       <form action="<?php $_PHP_SELF; ?>" method="POST">
         Vardas: <input type="text" name="name" />
         Pavardė: <input type="text" name="surname" />
         <input type="submit" name="submit" value="submit">
       </form>
       <?php
-    }
-?>
+    }?>
   </body>
 </html>
