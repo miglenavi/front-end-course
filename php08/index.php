9a329f193d9eb298ea0b59d5a2fe8988b003a442
Migle <mig.navickaite@gmail.com>
2018-12-03 00:20:26 +0200

    Pridėtas pataisytas PHP8

--- php08/index.php
+++ php08/index.php
@@ -1,25 +1,21 @@
 <?php
 //The first task
- $april = [4, 3, 9, 19, 19, 9, 12, 20, 24, 20, 12, 14, 18, 21, 20, 23, 16, 16, 15,
-19, 19, 17, 17, 15, 12, 13, 13, 15, 19, 21];
+$april = array(-4, 3, 9, 19, 19, 9, 12, 20, 24, 20, 12, 14, 18, 21, 20, 23, 16, 16, 15,
+19, 19, 17, 17, 15, 12, 13, 13, 15, 19, 21);
 
-//The second one
-foreach ($april as $april) {
- $april = round($april);
-}
-print_r($april);
-
+//The second one --- wrong
+$avarage = array_sum($april)/count($april);
+print_r(round($avarage, 0));
 echo "<br>";
 echo "<br>";
 
-//`the third one`
-$april = [4, 3, 9, 19, 19, 9, 12, 20, 24, 20, 12, 14, 18, 21, 20, 23, 16, 16, 15,
-19, 19, 17, 17, 15, 12, 13, 13, 15, 19, 21];
-rsort($april);
+//`the third one`--- numbers cannot repeat
+asort($april);
+$arranged=array_unique($april);
 
-$warm = count($april);
+$warm = count($arranged);
 for($x = 0; $x < 5; $x++) {
-  $highTemp=array_slice($april,0,5);
+  $highTemp=array_slice($arranged,0,5);
     echo $highTemp[$x];
     echo "<br>";
 }
@@ -27,9 +23,9 @@ for($x = 0; $x < 5; $x++) {
 echo "<br>";
 echo "<br>";
 
-$cold = count($april);
+$cold = count($arranged);
 for($x = 0; $x < 5; $x++) {
-  $lowTemp=array_slice($april,-5,5);
+  $lowTemp=array_slice($arranged,-5,5);
     echo $lowTemp[$x];
     echo "<br>";
 }
